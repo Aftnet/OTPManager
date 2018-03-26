abd3d688ddf70fa673411baf3f8a4570ffc79822
Alberto <claudiuslollarius@gmail.com>
2018-03-26 09:11:44 +0900

    Logic

--- OTPManager.iOS/Views/CodesDisplayView.cs
+++ OTPManager.iOS/Views/CodesDisplayView.cs
@@ -4,35 +4,31 @@ using MvvmCross.iOS.Views.Presenters.Attributes;
 using OTPManager.Shared.ViewModels;
 using UIKit;
 
-namespace OTPManager.iOS.Views
+[MvxRootPresentation(WrapInNavigationController = true)]
+public partial class CodesDisplayView : MvxViewController<CodesDisplayViewModel>
 {
-    [MvxRootPresentation(WrapInNavigationController = true)]
-    public partial class CodesDisplayView : MvxViewController<CodesDisplayViewModel>
+    public CodesDisplayView() : base("CodesDisplayView", null)
     {
-        public CodesDisplayView() : base("CodesDisplayView", null)
-        {
-            
-        }
 
-        public override void ViewDidLoad()
-        {
-            base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+    }
 
-            var addManualButton = new UIBarButtonItem(UIBarButtonSystemItem.Add);
-            var addQRImage = new UIBarButtonItem(UIBarButtonSystemItem.Camera);
-            NavigationItem.RightBarButtonItems = new[] { addManualButton, addQRImage };
+    public override void ViewDidLoad()
+    {
+        base.ViewDidLoad();
+        // Perform any additional setup after loading the view, typically from a nib.
 
-            this.CreateBinding(ProgressBar).To<CodesDisplayViewModel>(d => d).WithConversion("CodesDisplayProgress").Apply();
-            this.CreateBinding(addManualButton).To<CodesDisplayViewModel>(d => d.CreateEntryManual).Apply();
-            this.CreateBinding(addQRImage).To<CodesDisplayViewModel>(d => d.CreateEntryQR).Apply();
-        }
+        var addManualButton = new UIBarButtonItem(UIBarButtonSystemItem.Add);
+        var addQRImage = new UIBarButtonItem(UIBarButtonSystemItem.Camera);
+        NavigationItem.RightBarButtonItems = new[] { addManualButton, addQRImage };
 
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
-        }
+        //this.CreateBinding(ProgressBar).To<CodesDisplayViewModel>(d => d).WithConversion("CodesDisplayProgress").Apply();
+        this.CreateBinding(addManualButton).To<CodesDisplayViewModel>(d => d.CreateEntryManual).Apply();
+        this.CreateBinding(addQRImage).To<CodesDisplayViewModel>(d => d.CreateEntryQR).Apply();
+    }
+
+    public override void DidReceiveMemoryWarning()
+    {
+        base.DidReceiveMemoryWarning();
+        // Release any cached data, images, etc that aren't in use.
     }
 }
-
