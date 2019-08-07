a52db5c4a98904abcc0a0a36deced3871ebb1e52
Alberto Fustinoni <claudiuslollarius@gmail.com>
2019-08-07 15:53:24 +0900

    Logic

---
+++ OTPManager.UWP/Platform/CustomSetup.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross;
+using MvvmCross.Platforms.Uap.Core;
+using MvvmCross.ViewModels;
+using Windows.UI.Xaml.Controls;
+using ZXing.Mobile;
+
+namespace OTPManager.UWP.Platform
+{
+    public class CustomSetup<TApplication> : MvxWindowsSetup<TApplication> where TApplication : class, IMvxApplication, new()
+    {
+        protected override void InitializeLastChance()
+        {
+            Mvx.IoCProvider.RegisterSingleton<IMobileBarcodeScanner>(() => new MobileBarcodeScanner
+            {
+                CustomOverlay = new Grid(),
+                UseCustomOverlay = true
+            });
+        }
+    }
+}
--- OTPManager.UWP/Views/AddGeneratorView.xaml.cs
+++ OTPManager.UWP/Views/AddGeneratorView.xaml.cs
@@ -1,13 +1,8 @@
-﻿using MvvmCross.Uwp.Views;
+﻿using MvvmCross.Platforms.Uap.Views;
 using OTPManager.Shared.ViewModels;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace OTPManager.UWP.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class AddGeneratorView : MvxWindowsPage
     {
         public AddGeneratorViewModel VM => ViewModel as AddGeneratorViewModel;
