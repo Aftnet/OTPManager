b0f5367f2516891f40b365621e73aec386cae8a8
Alberto Fustinoni <claudiuslollarius@gmail.com>
2019-08-08 17:45:25 +0900

    Logic

---
+++ OTPManager.Android/App.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Runtime;
+using MvvmCross.Platforms.Android.Views;
+using System;
+
+namespace OTPManager.Android
+{
+    [Application]
+    public class App : MvxAndroidApplication<CustomSetup<Shared.App>, Shared.App>
+    {
+        public App(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+        }
+    }
+}
