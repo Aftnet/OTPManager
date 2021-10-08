315a73804a4becc9a6a7c3198fe128f09ccfcf94
Alberto Fustinoni <alberto@aftnet.net>
2021-10-08 18:25:41 +0900

    Logic

--- OTPManager.iOS/Platform/CustomSetup.cs
+++ OTPManager.iOS/Platform/CustomSetup.cs
@@ -1,4 +1,5 @@
-﻿using MvvmCross;
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.IoC;
 using MvvmCross.Platforms.Ios.Core;
 using MvvmCross.ViewModels;
 using ZXing.Mobile;
@@ -7,11 +8,21 @@ namespace OTPManager.iOS.Platform
 {
     public class CustomSetup<TApplication> : MvxIosSetup<TApplication> where TApplication : class, IMvxApplication, new()
     {
-        protected override void InitializeLastChance()
+        protected override ILoggerProvider CreateLogProvider()
         {
-            base.InitializeLastChance();
+            return default(ILoggerProvider);
+        }
 
-            Mvx.IoCProvider.RegisterSingleton<IMobileBarcodeScanner>(() => new MobileBarcodeScanner
+        protected override ILoggerFactory CreateLogFactory()
+        {
+            return default(ILoggerFactory);
+        }
+
+        protected override void InitializeLastChance(IMvxIoCProvider iocProvider)
+        {
+            base.InitializeLastChance(iocProvider);
+
+            iocProvider.RegisterSingleton<IMobileBarcodeScanner>(() => new MobileBarcodeScanner
             {
                 //CustomOverlay = new UIView(),
                 UseCustomOverlay = false
